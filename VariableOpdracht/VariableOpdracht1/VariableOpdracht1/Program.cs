b8cfb00dcd572165f3f306677cc1f5c5fec22872
Owen Stas <34884@ma-web.nl>
2023-02-24 11:46:39 +0100

    var

---
+++ VariableOpdracht/VariableOpdracht1/VariableOpdracht1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariableOpdracht1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //maak hier 3 local variables aan:
+            //1 type=string, naam: name, waarde je favoriete rpg character
+            //2 type=int, naam: hp, een waarde van hoeveel health dit character heeft
+            //3 type=int, naam: str, een waarde van hoe sterk dit character is
+            string name = "frank";
+            int hp = 30;
+            int str = 4;
+
+            //nu zou de statement hieronder moeten werken
+            Console.WriteLine($"{name} {hp} {str}");
+        }
+    }
+
+}
---
+++ VariableOpdracht/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class TwitchStream
+    {
+        //maak hier class variables aan:
+        // type=string, naam: name,
+        // type=string, naam: lastStreamName
+        // type=Game, naam: streamGame
+        // type=int, naam: followers
+        // type=bool, naam: automod
+
+        internal bool automod;
+        internal string name;
+        internal int followers;
+        internal string LastStreamName;
+        internal Game streamgame;
+    }
+}
