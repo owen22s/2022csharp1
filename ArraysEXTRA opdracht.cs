de75ebd90222ff2f3f9145330833f04a87f1aaea
Owen Stas <34884@ma-web.nl>
2023-03-17 12:47:59 +0100

    les 5B extra

---
+++ ArraysEXTRA opdracht.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50, 60 };
+        int sum = SumArray(numbers);
+        Console.WriteLine(sum);
+        Console.WriteLine(sum == 210);
+    }
+
+    static int SumArray(int[] arr)
+    {
+        int sum = 0;
+        foreach (int num in arr)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}
