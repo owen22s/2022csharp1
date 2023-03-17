eac0b3fbacaf9029a3dcfcfe650f0698933a4d14
Owen Stas <34884@ma-web.nl>
2023-03-17 12:21:42 +0100

    les 5B

---
+++ ArrayOpdracht/ArrayOpdracht2/ArrayOpdracht2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrayOpdracht2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo, Wereld!");
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6 }; //maak hier een nieuwe array en vul deze met getallen; zorg dat je minimaal 6 getallen hebt
+
+            //kijk nu wat er gebeurt als je het programma runt
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ ArrayOpdracht/ArrayOpdracht3/ArrayOpdracht3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ArrayOpdracht3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string[] nameparts = new string[] { "mar", "chu" , "io", "li",  "pika", "nk" };
+
+
+            Console.WriteLine(nameparts[0] + nameparts[2]);
+            Console.WriteLine(nameparts[4] + nameparts[1]);
+            Console.WriteLine(nameparts[3] + nameparts[5]);
+            Console.WriteLine(nameparts[1] + nameparts[0]);
+            Console.WriteLine(nameparts[3] + nameparts[5] + nameparts[2]);
+
+            //maak nu de volgende namen en schrijf het naar de console:
+
+            //link
+
+            //pikachu
+
+            //linkio
+
+            //chumar
+        }
+    }
+}
